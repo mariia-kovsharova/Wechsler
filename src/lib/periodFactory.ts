20ab484643b36e5e27981229b515a5bf000740b1
fiorsaoirse <solovyanovamaria@yandex.ru>
2022-04-12 20:53:40 +0300

    +

--- src/lib/periodFactory.ts
+++ src/lib/periodFactory.ts
@@ -1,4 +1,4 @@
-import differenceInDays from 'date-fns/differenceInDays';
+import { differenceInMonths } from 'date-fns';
 import {
     FiveYearsFromEightToElevenMonths,
     FiveYearsFromFourToSevenMonths, FiveYearsFromZeroToThreeMonths, Period,
@@ -22,57 +22,57 @@ import {
 import { StudentDate, TestDate } from '../domain/types';
 
 export const periodFactory = (testDate: TestDate, studentBirthDate: StudentDate): Period | null => {
-    const daysCount = differenceInDays(testDate, studentBirthDate);
+    const monthsCount = differenceInMonths(testDate, studentBirthDate);
 
     switch (true) {
         // 5 years 0 months - 5 years 3 months
-        case (daysCount >= 1825 && daysCount <= 1919):
+        case (monthsCount >= 60 && monthsCount < 64):
             return new FiveYearsFromZeroToThreeMonths();
         // 5 years 4 months - 5 years 7 months
-        case (daysCount >= 1920 && daysCount <= 2039):
+        case (monthsCount >= 64 && monthsCount < 68):
             return new FiveYearsFromFourToSevenMonths();
         // 5 years 8 months - 5 years 11 months
-        case (daysCount >= 2040 && daysCount <= 2159):
+        case (monthsCount >= 68 && monthsCount < 72):
             return new FiveYearsFromEightToElevenMonths();
 
         // 6 years 0 months - 6 years 3 months
-        case (daysCount >= 2160 && daysCount <= 2279):
+        case (monthsCount >= 72 && monthsCount < 76):
             return new SixYearsFromZeroToThreeMonths();
         // 6 years 4 months - 6 years 7 months
-        case (daysCount >= 2280 && daysCount <= 2399):
+        case (monthsCount >= 76 && monthsCount < 80):
             return new SixYearsFromFourToSevenMonths();
         // 6 years 8 months - 6 years 11 months
-        case (daysCount >= 2400 && daysCount <= 2519):
+        case (monthsCount >= 80 && monthsCount < 84):
             return new SixYearsFromEightToElevenMonths();
 
         // 7 years 0 months - 7 years 3 months
-        case (daysCount >= 2520 && daysCount <= 2639):
+        case (monthsCount >= 84 && monthsCount < 88):
             return new SevenYearsFromZeroToThreeMonths();
         // 7 years 4 months - 7 years 7 months
-        case (daysCount >= 2640 && daysCount <= 2759):
+        case (monthsCount >= 88 && monthsCount < 92):
             return new SevenYearsFromFourToSevenMonths();
         // 7 years 8 months - 7 years 11 months
-        case (daysCount >= 2760 && daysCount <= 2879):
+        case (monthsCount >= 92 && monthsCount < 96):
             return new SevenYearsFromEightToElevenMonths();
 
         // 8 years 0 months - 8 years 3 months
-        case (daysCount >= 2880 && daysCount <= 2999):
+        case (monthsCount >= 96 && monthsCount < 100):
             return new EightYearsFromZeroToThreeMonths();
         // 8 years 4 months - 8 years 7 months
-        case (daysCount >= 3000 && daysCount <= 3119):
+        case (monthsCount >= 100 && monthsCount < 104):
             return new EightYearsFromFourToSevenMonths();
         // 8 years 8 months - 8 years 11 months
-        case (daysCount >= 3120 && daysCount <= 3239):
+        case (monthsCount >= 104 && monthsCount < 108):
             return new EightYearsFromEightToElevenMonths();
 
         // 9 years 0 months - 9 years 3 months
-        case (daysCount >= 3240 && daysCount <= 3359):
+        case (monthsCount >= 108 && monthsCount < 112):
             return new NineYearsFromZeroToThreeMonths();
         // 9 years 4 months - 9 years 7 months
-        case (daysCount >= 3360 && daysCount <= 3479):
+        case (monthsCount >= 112 && monthsCount < 116):
             return new NineYearsFromFourToSevenMonths();
         // 9 years 8 months - 9 years 11 months
-        case (daysCount >= 3480 && daysCount <= 3599):
+        case (monthsCount >= 116 && monthsCount < 120):
             return new NineYearsFromEightToElevenMonths();
         default:
             return null;
