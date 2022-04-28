0ba28867b93afc55a38895150351f687857698b4
fiorsaoirse <solovyanovamaria@yandex.ru>
2022-04-28 22:19:26 +0300

    +

--- src/domain/entities/period/index.ts
+++ src/domain/entities/period/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export * from './5. five-years';
 export * from './6. six-years';
 export * from './7. seven-years';
@@ -10,11 +9,4 @@ export * from './12. twelve-years';
 export * from './13. thirteen-years';
 export * from './14. fourteen-years';
 export * from './15. fifteen-years';
-=======
-export * from './five-years';
-export * from './six-years';
-export * from './seven-years';
-export * from './eight-years';
-export * from './nine-years';
->>>>>>> 90e30d8d3658fad6d0bebc7592736fd011b3b81b
 export * from './period';
